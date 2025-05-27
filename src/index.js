167dd55142798788b3780c5070d25ebaee0217e4
Lintelee <103197176+Lintelee@users.noreply.github.com>
2025-05-28 00:18:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// 如果您想要註冊 Service Worker (PWA功能)
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// 如果您想要讓應用離線工作，可以取消下面的註釋
-// serviceWorkerRegistration.register();
